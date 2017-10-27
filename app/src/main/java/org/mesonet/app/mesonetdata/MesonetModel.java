fd208245f485077626eb6bc91f3cfb5525152346
KairisCharm <kairis_charm@hotmail.com>
2017-10-27 15:53:27 -0500

    Mesonet model

---
+++ app/src/main/java/org/mesonet/app/mesonetdata/MesonetModel.java
@@ -0,0 +1,43 @@
+package org.mesonet.app.mesonetdata;
+
+
+
+public class MesonetModel
+{
+    Integer STNM;
+    Long TIME;
+    Number RELH;
+    Number TAIR;
+    Number WSPD;
+    Number WVEC;
+    Number WDIR;
+    Number WDSD;
+    Number WSSD;
+    Number WMAX;
+    Number RAIN;
+    Number PRES;
+    Number SRAD;
+    Number TA9M;
+    Number WS2M;
+    Number TS10;
+    Number TB10;
+    Number TS05;
+    Number TS25;
+    Number TS60;
+    Number TR05;
+    Number TR25;
+    Number TR60;
+    Number RAIN_24H;
+    Number RAIN_24;
+    String STID;
+    Integer YEAR;
+    Integer MONTH;
+    Integer DAY;
+    Integer HOUR;
+    Integer MINUTE;
+    Integer SECOND;
+
+
+
+
+}
